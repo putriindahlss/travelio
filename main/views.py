76e767317cbb9280816bef9e04ea4d10e3acd938
putriindahlss <putriindah2004@gmail.com>
2023-11-23 13:10:36 +0700

    mau deploy

--- main/views.py
+++ main/views.py
@@ -103,6 +103,7 @@ def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+@csrf_exempt
 def register(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -125,6 +126,7 @@ def register(request):
     else:
         return render(request, 'register.html')
 
+@csrf_exempt
 def login_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -140,6 +142,7 @@ def login_user(request):
     context = {}
     return render(request, 'login.html', context)
 
+@csrf_exempt
 def logout_user(request):
     logout(request)
     response = HttpResponseRedirect(reverse('main:login'))
